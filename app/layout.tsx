176772f2989ad85c4e30d604c3305bf96e1161bb
Fernandosarquiz <fernandosarquiz@gmail.com>
2023-10-05 20:00:04 -0400

    gg

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,12 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
-import { Metadata } from 'next'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Inventario sistema Pos v1',
   description: 'el mejor inventario para tu negocio',
 
--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,16 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
+    
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        
+        
+          <UserButton afterSignOutUrl='/' />
+        
+        
+        
         <div>
+          
           <h1> Pantalla de Inicio</h1>
 
           <SideNavbar />
